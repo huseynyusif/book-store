48a6b04fa35297feb95588af076b345412be1867
Huseyn <59554521+huseynyusif@users.noreply.github.com>
2024-04-13 13:33:51 +0400

    2nd commit

--- src/main/java/com/example/backend002/controller/BookController.java
+++ src/main/java/com/example/backend002/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.example.backend002.controller;
 
+import com.example.backend002.service.BookService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class BookController {
 
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
 }
